48b7997a37f78a25b55b99a289c8a36f54fbbf22
Domenico <domedfd@gmail.com>
2019-11-07 16:55:03 -0300

    http com build0

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -3,6 +3,7 @@ import { reducer as formReducer } from "redux-form";
 import { reducer as toastrReducer } from "react-redux-toastr";
 
 import DashboardReducer from "../dashboard/dashboardReducer";
+
 import TabReducer from "../common/tab/tabReducer";
 import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 import AuthReducer from "../auth/authReducer";
