aa65ca7ae870e4821bb0b0f65d840e9061e573ef
Domenico <domedfd@gmail.com>
2019-11-08 10:17:09 -0300

    problema de url e eliminado rest from que nao usava

--- src/dashboard/dashboardActions.js
+++ src/dashboard/dashboardActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3003/api";
+const BASE_URL = "https://dg-b.herokuapp.com/api";
 
 export function getSummary() {
   const request = axios.get(`${BASE_URL}/billingCycles/summary`);
