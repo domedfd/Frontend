e8992241c78d409ec13ef2ae5938e75ff2164d00
Domenico <domedfd@gmail.com>
2019-11-11 13:31:52 -0300

    fixed problems on heroku and varible on env

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,10 @@
 export default {
-  API_URL: process.env.NODE_ENV
-    ? "https://dg-b.herokuapp.com/api"
-    : "http://localhost:3003/api",
-  OAPI_URL: process.env.NODE_ENV
-    ? "https://dg-b.herokuapp.com/oapi"
-    : "http://localhost:3003/oapi"
+  API_URL:
+    process.env.NODE_ENV !== "development"
+      ? "https://dg-b.herokuapp.com/api"
+      : "http://localhost:3003/api",
+  OAPI_URL:
+    process.env.NODE_ENV !== "development"
+      ? "https://dg-b.herokuapp.com/oapi"
+      : "http://localhost:3003/oapi"
 };
