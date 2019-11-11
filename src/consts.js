561bccc96ae3d6d98814a2b5331662b39dbb1a33
Domenico <domedfd@gmail.com>
2019-11-11 13:25:47 -0300

    agora vai porra

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,8 @@
 export default {
-  API_URL: "https://dg-b.herokuapp.com/api",
-  OAPI_URL: "https://dg-b.herokuapp.com/oapi",
-  DOMENICO: process.env.NODE_ENV ? "Setada" : "Nao setada",
-  PORTA: "vamosver"
+  API_URL: process.env.NODE_ENV
+    ? "https://dg-b.herokuapp.com/api"
+    : "http://localhost:3003/api",
+  OAPI_URL: process.env.NODE_ENV
+    ? "https://dg-b.herokuapp.com/oapi"
+    : "http://localhost:3003/oapi"
 };
