4ab6214c80b0b88c9c338ec3b81cdbeb8ebc4e99
Domenico <domedfd@gmail.com>
2019-11-11 12:56:47 -0300

    verifica

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export default {
   API_URL: "https://dg-b.herokuapp.com/api",
   OAPI_URL: "https://dg-b.herokuapp.com/oapi",
-  DOMENICO: process.env.DOMENICO
-  PORTA: process.env.PORT
+  DOMENICO: process.env.DOMENICO ? process.env.DOMENICO : "Vacio",
+  PORTA: process.env.PORT ? process.env.PORT : "Vacio"
 };
