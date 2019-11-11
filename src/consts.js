0563203c149834e0639eb8e672c893e1303d93a7
Domenico <domedfd@gmail.com>
2019-11-11 13:16:50 -0300

    ver se seta

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export default {
   API_URL: "https://dg-b.herokuapp.com/api",
   OAPI_URL: "https://dg-b.herokuapp.com/oapi",
-  DOMENICO: process.env.DOMENICO ? process.env.DOMENICO : "Vacio",
-  PORTA: process.env
+  DOMENICO: process.env.PUBLIC_URL ? "Setada" : "Nao setada",
+  PORTA: "vamosver"
 };
